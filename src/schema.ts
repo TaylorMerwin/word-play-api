8159de314366663ab82cf25b80e62455ef4719db
Taylor <taylor.merwin@gmail.com>
2024-06-21 11:29:50 -0700

    eslint and prettier setup

--- src/schema.ts
+++ src/schema.ts
@@ -1,28 +1,39 @@
-import { integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable('users_table', {
-  userId: serial('user_id').primaryKey(),
-  userName: varchar('user_name').notNull(),
-  email: varchar('email').notNull().unique(),
-  passwordHash: varchar('password_hash').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
+export const usersTable = pgTable("users_table", {
+  userId: serial("user_id").primaryKey(),
+  userName: varchar("user_name").notNull(),
+  email: varchar("email").notNull().unique(),
+  passwordHash: varchar("password_hash").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
-export const entryTable = pgTable('writing_entry_table', {
-  entryId: serial('entry_id').primaryKey(),
-  userId: integer('user_id').notNull().references(()=> usersTable.userId),
-  entryText: text('entry_text').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  wordCount: integer('word_count').notNull(),
-  writingPromptId: integer('writing_prompt_id').notNull().references(()=> promptTable.promptId),
+export const entryTable = pgTable("writing_entry_table", {
+  entryId: serial("entry_id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => usersTable.userId),
+  entryText: text("entry_text").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  wordCount: integer("word_count").notNull(),
+  writingPromptId: integer("writing_prompt_id")
+    .notNull()
+    .references(() => promptTable.promptId),
 });
 
-export const promptTable = pgTable('writing_prompt_table', {
-  promptId: serial('prompt_id').primaryKey(),
-  promptText: text('prompt_text').notNull(),
-  genre: varchar('genre').notNull(),
-  theme: varchar('theme').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
+export const promptTable = pgTable("writing_prompt_table", {
+  promptId: serial("prompt_id").primaryKey(),
+  promptText: text("prompt_text").notNull(),
+  genre: varchar("genre").notNull(),
+  theme: varchar("theme").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
@@ -33,4 +44,3 @@ export type SelectEntry = typeof entryTable.$inferSelect;
 
 export type InsertPrompt = typeof promptTable.$inferInsert;
 export type SelectPrompt = typeof promptTable.$inferSelect;
-
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { db } from './db';
-import { GoogleGenerativeAI, GoogleGenerativeAIError, HarmBlockThreshold, HarmCategory } from '@google/generative-ai';
-import { promptTable } from './schema';
-import { InsertPrompt } from './schema';
+import express from "express";
+import dotenv from "dotenv";
+import { db } from "./db";
+import {
+  GoogleGenerativeAI,
+  GoogleGenerativeAIError,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
+import { promptTable } from "./schema";
+import { InsertPrompt } from "./schema";
 
 dotenv.config();
 
@@ -30,22 +35,23 @@ const generationConfig = {
 };
 
 async function generateText(genre: string, theme: string) {
-
-  let parts = [
-    {text: "You are an expert author and story teller with experience writing stories of all different genres and themes. Your job is to produce a unique prompt to be used as the basis to write a short story. The prompt is to be structured as either a question, a statement or a scenario and should not be longer than three sentences. This story will involve a specified theme and genre."},
-    {text: `genre: ${genre}`},
-    {text: `theme: ${theme}`},
-    {text: "prompt: "},
+  const parts = [
+    {
+      text: "You are an expert author and story teller with experience writing stories of all different genres and themes. Your job is to produce a unique prompt to be used as the basis to write a short story. The prompt is to be structured as either a question, a statement or a scenario and should not be longer than three sentences. This story will involve a specified theme and genre.",
+    },
+    { text: `genre: ${genre}` },
+    { text: `theme: ${theme}` },
+    { text: "prompt: " },
   ];
   // Generate the content
   const result = await model.generateContent({
-    contents: [{ role: "user", parts}],
+    contents: [{ role: "user", parts }],
     generationConfig,
   });
 
   //Parse the result
   try {
-    const  parsedResponse = JSON.parse(result.response.text());
+    const parsedResponse = JSON.parse(result.response.text());
 
     // Store the generated prompt in the database using Drizzle ORM
     const newPrompt: InsertPrompt = {
@@ -55,23 +61,25 @@ async function generateText(genre: string, theme: string) {
     };
     console.log("New Prompt Created: " + newPrompt);
     console.log("Inserting prompt into database...");
-    const insertedPrompt = await db.insert(promptTable).values(newPrompt).returning();
+    const insertedPrompt = await db
+      .insert(promptTable)
+      .values(newPrompt)
+      .returning();
     parsedResponse.promptId = insertedPrompt[0].promptId;
 
     return parsedResponse;
-  }
-  catch (error) {
-    throw new GoogleGenerativeAIError("An error occurred while parsing the response");
+  } catch (error) {
+    throw new GoogleGenerativeAIError(
+      "An error occurred while parsing the response",
+    );
   }
 }
 
-
-app.get('/', async (req, res) => {
-    res.send("hello world");
+app.get("/", async (req, res) => {
+  res.send("hello world");
 });
 
-
-app.post('/generate', async (req, res) => {
+app.post("/generate", async (req, res) => {
   console.log("Generating text...");
   try {
     const { genre, theme } = req.body;
